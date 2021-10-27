e837705d7efac05553f604923992f14329e63668
myc37 <nvjn37@gmail.com>
2021-10-27 12:10:50 +0800

    jwt auth

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,7 @@ namespace CarparkWebAPI.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
---
+++ Service/TokenService.cs
@@ -0,0 +1,60 @@
+﻿using CarparkWebAPI.ViewModels;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarparkWebAPI.Service
+{
+    public interface ITokenService
+    {
+        string BuildToken(string key, string issuer, string audience, LoginViewModel user);
+        bool ValidateToken(string key, string issuer, string audience, string token);
+    }
+    public class TokenService : ITokenService
+    {
+        private const double EXPIRY_DURATION_MINUTES = 30;
+
+        public string BuildToken(string key, string issuer, string audience, LoginViewModel user)
+        {
+            var claimsList = new[]
+            {
+                new Claim("EMAIL", user.Email),
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+            var tokenDescriptor = new JwtSecurityToken(issuer, audience, claimsList,
+                expires: DateTime.Now.AddMinutes(EXPIRY_DURATION_MINUTES), signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+        public bool ValidateToken(string key, string issuer, string audience, string token)
+        {
+            var mySecret = Encoding.UTF8.GetBytes(key);
+            var mySecurityKey = new SymmetricSecurityKey(mySecret);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                tokenHandler.ValidateToken(token,
+                new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    IssuerSigningKey = mySecurityKey,
+                }, out SecurityToken validatedToken);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
